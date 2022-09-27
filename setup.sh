2f541a0cd7c911f3d71411bc9eedb75c40da7999
Michael Kramer <michaelkramer@users.noreply.github.com>
2022-09-27 11:23:39 -0400

    Add Comments to the scripts

--- setup.sh
+++ setup.sh
@@ -3,16 +3,25 @@ yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebr
 brew install git
 brew install node
 brew install git node yarn
+# password protection app
 brew cask install authy
 brew cask install docker
 brew cask install dropbox
 brew cask install google-backup-and-sync
 brew cask install google-chrome
+# terminal app
 brew cask install iterm2
+# auto suggest for terminal
+brew cask install fig
+# screen recorder
 brew cask install kap
+# Postgrest Management Console
 brew cask install postico
+# Chat app
 brew cask install slack
+# IDE
 brew cask install visual-studio-code
+# video viewer
 brew cask install vlc
 
 # Copy dotfiles to user's home directory
